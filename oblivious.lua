ca4c0583f086c2fe6ffafaed514a82e5f4e13861
Antoine Gersant <antoine.gersant@lesforges.org>
2023-08-14 01:38:53 -0700

    Formatting

--- oblivious.lua
+++ oblivious.lua
@@ -12,25 +12,18 @@ local exclude = {
 		{ "c_main", "c_user_point", "e_text_leaguepoint" },
 	},
 	-- Battle settings > character settings > title settings preview
-	MatchingFighterSetting = {
-		{ "c_main", "p_Group_0_switch_show", "c_list", "p_MatchingTitleSetting_1_h", "p_Group_0_switch", "c_detail",
-			"c_preview", "p_FightersProfileBanner_0", "p_SmallRankIcon_" }
-	},
+	MatchingFighterSetting = { { "c_main", "p_Group_0_switch_show", "c_list", "p_MatchingTitleSetting_1_h", "p_Group_0_switch", "c_detail", "c_preview", "p_FightersProfileBanner_0", "p_SmallRankIcon_" } },
 
 	CustomRoomRoom = {
 		-- Custom room participant rows
-		{ "c_main", "p_Group_base_h", "p_Group_communication_v", "p_ScrollList_member_v", "p_CustomRoomMemberBanner_",
-			"p_LargeRankIcon_0" },
-		{ "c_main", "p_Group_base_h", "p_Group_communication_v", "p_ScrollList_member_v", "item%d+",
-			"p_LargeRankIcon_0" },
+		{ "c_main", "p_Group_base_h", "p_Group_communication_v", "p_ScrollList_member_v", "p_CustomRoomMemberBanner_", "p_LargeRankIcon_0" },
+		{ "c_main", "p_Group_base_h", "p_Group_communication_v", "p_ScrollList_member_v", "item%d+", "p_LargeRankIcon_0" },
 		-- Custom room slot
-		{ "c_main", "p_Group_base_h", "p_Group_table_v", "p_SimpleList_SingleTable_v", "p_CustomRoomSimpleTable_%d",
-			"p_CustomRoomMemberBanner_player%d", "p_LargeRankIcon_0" },
+		{ "c_main", "p_Group_base_h", "p_Group_table_v", "p_SimpleList_SingleTable_v", "p_CustomRoomSimpleTable_%d", "p_CustomRoomMemberBanner_player%d", "p_LargeRankIcon_0" },
 	},
 	-- Custom room slot popup
 	CustomRoomTableDetail = {
-		{ "c_main", "p_Group_all_v", "p_CustomRoomSimpleTable_simpleTable_h", "p_CustomRoomMemberBanner_player%d",
-			"p_LargeRankIcon_0" }
+		{ "c_main", "p_Group_all_v", "p_CustomRoomSimpleTable_simpleTable_h", "p_CustomRoomMemberBanner_player%d", "p_LargeRankIcon_0" }
 	},
 
 	-- CFN > profile > stats/history/clubs banner
@@ -41,19 +34,15 @@ local exclude = {
 	CFNFighterProfileChildPlayTab3 = {
 		{ "c_main", "p_Group_0_switch", "p_ScrollList_chara_v", "p_CharacterUseBanner", "p_SmallRankIcon_" },
 		{ "c_main", "p_Group_0_switch", "p_ScrollList_chara_v", "p_CharacterUseBanner", "e_text_lp_num" },
-		{ "c_main", "p_Group_0_switch", "p_ScrollList_chara_v", "item%d",               "p_SmallRankIcon_" },
-		{ "c_main", "p_Group_0_switch", "p_ScrollList_chara_v", "item%d",               "e_text_lp_num" },
+		{ "c_main", "p_Group_0_switch", "p_ScrollList_chara_v", "item%d", "p_SmallRankIcon_" },
+		{ "c_main", "p_Group_0_switch", "p_ScrollList_chara_v", "item%d", "e_text_lp_num" },
 	},
 	-- CFN > profile > history rows
 	CFNFighterProfileChildBattleLog = {
-		{ "c_main", "p_ScrollList_0_v_noloop", "p_ReplayBannerProfile_", "p_ReplayBannerPlayerInfoShort_%dP",
-			"p_SmallRankIcon_" },
-		{ "c_main", "p_ScrollList_0_v_noloop", "p_ReplayBannerProfile_", "p_ReplayBannerPlayerInfoShort_%dP",
-			"e_text_lp_num" },
-		{ "c_main", "p_ScrollList_0_v_noloop", "item%d", "p_ReplayBannerPlayerInfoShort_%dP",
-			"p_SmallRankIcon_" },
-		{ "c_main", "p_ScrollList_0_v_noloop", "item%d", "p_ReplayBannerPlayerInfoShort_%dP",
-			"e_text_lp_num" },
+		{ "c_main", "p_ScrollList_0_v_noloop", "p_ReplayBannerProfile_", "p_ReplayBannerPlayerInfoShort_%dP", "p_SmallRankIcon_" },
+		{ "c_main", "p_ScrollList_0_v_noloop", "p_ReplayBannerProfile_", "p_ReplayBannerPlayerInfoShort_%dP", "e_text_lp_num" },
+		{ "c_main", "p_ScrollList_0_v_noloop", "item%d", "p_ReplayBannerPlayerInfoShort_%dP", "p_SmallRankIcon_" },
+		{ "c_main", "p_ScrollList_0_v_noloop", "item%d", "p_ReplayBannerPlayerInfoShort_%dP", "e_text_lp_num" },
 	},
 	-- CFN > profile > history > match popup, CFN > replays > popup with replay details
 	CFNReplayInfo = {
@@ -62,48 +51,22 @@ local exclude = {
 	},
 	-- CFN > replays > replay row
 	CFNReplayChildList = {
-		{ "c_main", "p_Group_root_switch", "p_ReplayList_", "p_ScrollList_multi_v", "p_ReplayBanner_",
-			"p_ReplayBannerPlayerInfo_%dP",
-			"e_text_lp_num" },
-		{ "c_main", "p_Group_root_switch", "p_ReplayList_", "p_ScrollList_multi_v", "p_ReplayBanner_",
-			"p_ReplayBannerPlayerInfo_%dP",
-			"c_league" },
-		{ "c_main", "p_Group_root_switch", "p_ReplayList_", "p_ScrollList_multi_v", "item%d",
-			"p_ReplayBannerPlayerInfo_%dP",
-			"e_text_lp_num" },
-		{ "c_main", "p_Group_root_switch", "p_ReplayList_", "p_ScrollList_multi_v", "item%d",
-			"p_ReplayBannerPlayerInfo_%dP",
-			"c_league" },
+		{ "c_main", "p_Group_root_switch", "p_ReplayList_", "p_ScrollList_multi_v", "p_ReplayBanner_", "p_ReplayBannerPlayerInfo_%dP", "e_text_lp_num" },
+		{ "c_main", "p_Group_root_switch", "p_ReplayList_", "p_ScrollList_multi_v", "p_ReplayBanner_", "p_ReplayBannerPlayerInfo_%dP", "c_league" },
+		{ "c_main", "p_Group_root_switch", "p_ReplayList_", "p_ScrollList_multi_v", "item%d", "p_ReplayBannerPlayerInfo_%dP", "e_text_lp_num" },
+		{ "c_main", "p_Group_root_switch", "p_ReplayList_", "p_ScrollList_multi_v", "item%d", "p_ReplayBannerPlayerInfo_%dP", "c_league" },
 	},
 	-- CFN > ranking > league points
 	CFNRankingLeaguePoint = {
 		{ "c_main", "p_RankingThumbnailLeaguePoint_", "p_LargeRankIcon_0" },
 		{ "c_main", "p_RankingThumbnailLeaguePoint_", "c_league_point" },
 		{ "c_main", "p_RankingThumbnailLeaguePoint_", "e_text_rank" },
-		{ "c_main", "p_Group_root_switch", "p_RankingListLeaguePoint", "p_ScrollList_ranking_v_noloop",
-			"p_FighterBannerRankingLeaguePoint_",
-			"p_FighterBannerRankingInfo_",
-			"p_LargeRankIcon_" },
-		{ "c_main", "p_Group_root_switch", "p_RankingListLeaguePoint", "p_ScrollList_ranking_v_noloop",
-			"p_FighterBannerRankingLeaguePoint_",
-			"p_FighterBannerRankingInfo_",
-			"e_text_lp" },
-		{ "c_main", "p_Group_root_switch", "p_RankingListLeaguePoint", "p_ScrollList_ranking_v_noloop",
-			"p_FighterBannerRankingLeaguePoint_",
-			"p_FighterBannerRankingInfo_",
-			"c_rank" },
-		{ "c_main", "p_Group_root_switch", "p_RankingListLeaguePoint", "p_ScrollList_ranking_v_noloop",
-			"item%d",
-			"p_FighterBannerRankingInfo_",
-			"p_LargeRankIcon_" },
-		{ "c_main", "p_Group_root_switch", "p_RankingListLeaguePoint", "p_ScrollList_ranking_v_noloop",
-			"item%d",
-			"p_FighterBannerRankingInfo_",
-			"e_text_lp" },
-		{ "c_main", "p_Group_root_switch", "p_RankingListLeaguePoint", "p_ScrollList_ranking_v_noloop",
-			"item%d",
-			"p_FighterBannerRankingInfo_",
-			"c_rank" },
+		{ "c_main", "p_Group_root_switch", "p_RankingListLeaguePoint", "p_ScrollList_ranking_v_noloop", "p_FighterBannerRankingLeaguePoint_", "p_FighterBannerRankingInfo_", "p_LargeRankIcon_" },
+		{ "c_main", "p_Group_root_switch", "p_RankingListLeaguePoint", "p_ScrollList_ranking_v_noloop", "p_FighterBannerRankingLeaguePoint_", "p_FighterBannerRankingInfo_", "e_text_lp" },
+		{ "c_main", "p_Group_root_switch", "p_RankingListLeaguePoint", "p_ScrollList_ranking_v_noloop", "p_FighterBannerRankingLeaguePoint_", "p_FighterBannerRankingInfo_", "c_rank" },
+		{ "c_main", "p_Group_root_switch", "p_RankingListLeaguePoint", "p_ScrollList_ranking_v_noloop", "item%d", "p_FighterBannerRankingInfo_", "p_LargeRankIcon_" },
+		{ "c_main", "p_Group_root_switch", "p_RankingListLeaguePoint", "p_ScrollList_ranking_v_noloop", "item%d", "p_FighterBannerRankingInfo_", "e_text_lp" },
+		{ "c_main", "p_Group_root_switch", "p_RankingListLeaguePoint", "p_ScrollList_ranking_v_noloop", "item%d", "p_FighterBannerRankingInfo_", "c_rank" },
 	},
 
 	-- In-match HUD
